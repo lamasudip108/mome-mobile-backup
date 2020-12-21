75062e9a04f30197c417f4738981cc46f4d54cbb
sudip <lamapeedus@gmail.com>
2020-12-22 00:11:12 +0545

    update home page + bottom tabs improvements

--- src/navigations/BottomTabNavigation.js
+++ src/navigations/BottomTabNavigation.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {View} from 'react-native';
 
 import SettingScreen from '@/screens/setting';
 import ProfileScreen from '@/screens/profile';
@@ -11,7 +13,7 @@ const Tab = createBottomTabNavigator();
 const BottomTabNavigation = () => {
 
     return (
-        <Tab.Navigator
+        /*<Tab.Navigator
             initialRouteName="Home"
             screenOptions={({route}) => ({
                 tabBarIcon: ({focused, color, size}) => {
@@ -25,6 +27,7 @@ const BottomTabNavigation = () => {
                     }
                     else if (route.name === 'Home') {
                         iconName = focused ? 'qr-code-outline' : 'qr-code-outline';
+                        size = 50;
                     }
                     return <Ionicons name={iconName} size={size} color={color}/>;
                 },
@@ -33,11 +36,81 @@ const BottomTabNavigation = () => {
                 activeTintColor: '#0000FF',
                 inactiveTintColor: 'gray',
                 showLabel: false,
+                style: { 
+                    backgroundColor: '#FFFFFF', 
+                    borderTopLeftRadius: 16,
+                    borderTopRightRadius: 16,
+                    borderWidth: 1,
+                    borderColor: '#FFFFFF',
+                    //paddingTop: 15,
+                    //paddingBottom: 15,
+                },
             }}
         >
             <Tab.Screen name="Setting" component={SettingScreen}/>
             <Tab.Screen name="Home" component={HomeScreen} />
             <Tab.Screen name="Profile" component={ProfileScreen}/>
+        </Tab.Navigator>*/
+        <Tab.Navigator
+          initialRouteName="Home"
+          tabBarOptions={{
+                activeTintColor: '#FFFFFF',
+                inactiveTintColor: 'gray',
+                showLabel: false,
+                style: { 
+                    backgroundColor: '#FFFFFF', 
+                    borderTopLeftRadius: 16,
+                    borderTopRightRadius: 16,
+                    borderWidth: 1,
+                    borderColor: '#FFFFFF',
+                    paddingTop: 15,
+                    paddingBottom: 15,
+                    height: 85,
+                },
+            }}
+        >
+          <Tab.Screen
+            name="Setting"
+            component={SettingScreen}
+            options={{
+              tabBarLabel: 'Setting',
+              tabBarIcon: ({ color, size }) => (
+               <Ionicons name="settings-outline" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarLabel: 'QRCode',
+              tabBarIcon: ({ color, size }) => (
+                <View
+                    style={{
+                      position: 'absolute',
+                      bottom: 20, // space from bottombar
+                      height: 58,
+                      width: 58,
+                      borderRadius: 58,
+                      backgroundColor: '#0000FF',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                }}>
+                <MaterialCommunityIcons name="qrcode-scan" color={color} size={size} />
+                </View>
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+              tabBarLabel: 'Profile',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="person-outline" color={color} size={size} />
+              ),
+            }}
+          />
         </Tab.Navigator>
     );
 };
