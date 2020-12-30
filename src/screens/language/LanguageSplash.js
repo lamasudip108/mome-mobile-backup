7c3572aaed661ffe6ffb31d43b2d5dd8991db6f5
Krishna Timilsina <krishna.timilsina@citytech.global>
2020-12-30 17:46:22 +0545

    icon launcher

--- src/screens/language/LanguageSplash.js
+++ src/screens/language/LanguageSplash.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
 import {Container, Content, Spinner} from 'native-base';
+import i18n from 'i18n-js';
 
 import {clearAsyncStorage, getAsyncStorage} from '@/utils/storageUtil';
 import {LANGUAGE_CHANGED} from '@/constants';
@@ -28,7 +29,7 @@ const LanguageSplashScreen = () => {
             <Content contentContainerStyle={{flex: 1, justifyContent:'center', alignItems:'center'}}>
                 <Spinner color='#0000FF'/>
 
-                <Text>Just a moment while we setup language.</Text>
+                <Text>{i18n.t('justamoment')}</Text>
             </Content>
         </Container>
     );
